c992565a5c1c6a7a5a0ac8bcd43e9601c4f58ff5
Vladislav Zimnikov <vladislavzimnikov@Vladislavs-MacBook-Pro.local>
2022-09-30 14:37:38 +0300

    Create type safe client - Updated client to be able to register queries and mutations upfront and then use special methods that will provide user with typesafety and autocompletion

--- src/client/client.ts
+++ src/client/client.ts
@@ -24,7 +24,30 @@ export const defaultOptions: RequestOptions = {
     endpoint: process.env.GRAPHQL_ENDPOINT || '/graphql'
 };
 
-export class Client {
+type QueriesMapRecord = {
+    query: Query<any, any, any>,
+    input?: any
+}
+
+type MutationsMapRecord = {
+    mutation: Mutation<any, any, any>,
+    input?: any
+}
+
+export class Client<
+    QueriesMap extends Record<string, QueriesMapRecord> = {},
+    MutationsMap extends Record<string, MutationsMapRecord> = {}
+> {
+    public queries: {
+        [K in keyof QueriesMap]: QueriesMap[K]
+    } = {} as {
+        [K in keyof QueriesMap]: QueriesMap[K]
+    };
+    public mutations: {
+        [K in keyof MutationsMap]: MutationsMap[K]
+    } = {} as {
+        [K in keyof MutationsMap]: MutationsMap[K]
+    }
     protected options: RequestOptions = defaultOptions;
 
     setEndpoint = (endpoint: string): void => {
@@ -80,6 +103,261 @@ export class Client {
 
         return parsedResponse;
     };
+
+    registerMutation<
+        N extends string,
+        RT,
+        A extends boolean,
+        MutationName extends string
+    >(
+        name: MutationName,
+        options: {
+            mutation: Mutation<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap,
+        MutationsMap & {
+            [K in MutationName]: {
+                mutation: typeof options.mutation,
+                input: never
+            }
+        }
+    >
+
+    registerMutation<
+        N extends string,
+        RT,
+        A extends boolean,
+        MutationName extends string
+    >(
+        name: MutationName,
+        options: {
+            mutation: () => Mutation<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap,
+        MutationsMap & {
+            [K in MutationName]: {
+                mutation: ReturnType<typeof options.mutation>,
+                input: never
+            }
+        }
+    >
+
+    registerMutation<
+        N extends string,
+        RT,
+        A extends boolean,
+        MutationName extends string,
+        Input extends any = never
+    >(
+        name: MutationName,
+        options: {
+            mutation: (input: Input) => Mutation<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap,
+        MutationsMap & {
+            [K in MutationName]: {
+                mutation: ReturnType<typeof options.mutation>,
+                input: Input
+            }
+        }
+    >
+
+    registerMutation<
+        N extends string,
+        RT,
+        A extends boolean,
+        MutationName extends string,
+        Input extends Record<string, any> = {}
+    >(
+        name: MutationName,
+        options: {
+            mutation: Mutation<N, RT, A>
+            | (() => Mutation<N, RT, A>)
+            | ((input: Input) => Mutation<N, RT, A>),
+        }
+    ) {
+        const newClient = new Client<
+            QueriesMap,
+            MutationsMap & {
+                [K in MutationName]: typeof options.mutation extends Mutation<any, any, any>
+                ? {
+                    mutation: Extract<typeof options.mutation, Mutation<any, any, any>>
+                }
+                : {
+                    mutation: ReturnType<Extract<typeof options.mutation, () => Mutation<N, RT, A>>>
+                }
+            }
+        >()
+
+        newClient.queries = this.queries;
+
+        newClient.mutations = {
+            ...this.mutations,
+            [name]: {
+                mutation: options.mutation
+            }
+        }
+
+        return newClient;
+    }
+
+    registerQuery<
+        N extends string,
+        RT,
+        A extends boolean,
+        QueryName extends string
+    >(
+        name: QueryName,
+        options: {
+            query: Query<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap & {
+            [K in QueryName]: {
+                query: typeof options.query,
+                input: never
+            }
+        },
+        MutationsMap
+    >
+
+    registerQuery<
+        N extends string,
+        RT,
+        A extends boolean,
+        QueryName extends string
+    >(
+        name: QueryName,
+        options: {
+            query: () => Query<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap & {
+            [K in QueryName]: {
+                query: ReturnType<typeof options.query>,
+                input: never
+            }
+        },
+        MutationsMap
+    >
+
+    registerQuery<
+        N extends string,
+        RT,
+        A extends boolean,
+        QueryName extends string,
+        Input extends any = never
+    >(
+        name: QueryName,
+        options: {
+            query: (input: Input) => Query<N, RT, A>,
+        }
+    ): Client<
+        QueriesMap & {
+            [K in QueryName]: {
+                query: ReturnType<typeof options.query>,
+                input: Input
+            }
+        },
+        MutationsMap
+    >
+
+    registerQuery<
+        N extends string,
+        RT,
+        A extends boolean,
+        QueryName extends string,
+        Input extends Record<string, any> = {}
+    >(
+        name: QueryName,
+        options: {
+            query: Query<N, RT, A>
+            | (() => Query<N, RT, A>)
+            | ((input: Input) => Query<N, RT, A>),
+        }
+    ) {
+        const newClient = new Client<
+            QueriesMap & {
+                [K in QueryName]: typeof options.query extends Query<any, any, any>
+                ? {
+                    query: Extract<typeof options.query, Query<any, any, any>>
+                }
+                : {
+                    query: ReturnType<Extract<typeof options.query, () => Query<N, RT, A>>>
+                }
+            },
+            MutationsMap
+        >()
+
+        newClient.queries = {
+            ...this.queries,
+            [name]: {
+                query: options.query
+            }
+        }
+
+        return newClient;
+    }
+
+    // registerMutation<N extends string, RT, A extends boolean>(
+    //     rawField: Mutation<N, RT, A>
+    // ): Client<
+    //     RegisteredQueries,
+    //     [...RegisteredMutations, typeof rawField]
+    // > {
+    //     return this;
+    // }
+
+    query<
+        QueryName extends keyof QueriesMap,
+    >(
+        queryTuple: QueriesMap[QueryName]['input'] extends never
+            ? [QueryName]
+            : [QueryName, QueriesMap[QueryName]['input']]
+    ): Promise<
+        DataType<QueriesMap[QueryName]['query']>
+    >
+
+    async query<
+        QueryName extends keyof QueriesMap,
+    >(
+        queryTuple: QueriesMap[QueryName]['input'] extends never
+            ? [QueryName]
+            : [QueryName, QueriesMap[QueryName]['input']]
+    ) {
+        const query = this.queries[queryTuple[0]].query
+
+        return this.post(query) as Promise<
+            DataType<QueriesMap[QueryName]['query']>
+        >
+    }
+
+    mutate<
+        MutationName extends keyof MutationsMap,
+    >(
+        queryTuple: MutationsMap[MutationName]['input'] extends never
+            ? [MutationName]
+            : [MutationName, MutationsMap[MutationName]['input']]
+    ): Promise<
+        DataType<MutationsMap[MutationName]['mutation']>
+    >
+
+    async mutate<
+        MutationName extends keyof MutationsMap,
+    >(
+        queryTuple: MutationsMap[MutationName]['input'] extends never
+            ? [MutationName]
+            : [MutationName, MutationsMap[MutationName]['input']]
+    ) {
+        const query = this.mutations[queryTuple[0]].mutation
+
+        return this.post(query) as Promise<
+            DataType<MutationsMap[MutationName]['mutation']>
+        >
+    }
 
     /**
      * Handles calculating and transforming fields on result
